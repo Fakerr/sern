f42d05b8ef984d15b804545af1a3ae1ffea173e8
Fakerr <berrahal.walid07@gmail.com>
2019-06-14 16:56:28 +0100

    update frontend structure

--- http/handlers/index.go
+++ http/handlers/index.go
@@ -7,15 +7,15 @@ import (
 	"github.com/Fakerr/sern/http/session"
 )
 
-var templates = template.Must(template.ParseFiles("static/logout.html", "static/index.html"))
+var templates = template.Must(template.ParseFiles("public/home.html", "ui/build/index.html"))
 
 // Hnadler for the main route
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	sess := session.Instance(r)
 
 	if sess.IsNew == true {
-		templates.ExecuteTemplate(w, "index.html", nil)
+		templates.ExecuteTemplate(w, "home.html", nil)
 		return
 	}
-	templates.ExecuteTemplate(w, "logout.html", nil)
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
